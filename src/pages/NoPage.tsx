0424f36e572113afbb1886c90e67c72cea74de92
romzy <romziqutbi@gmail.com>
2023-12-27 23:13:50 +0700

    fix: page not found

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,4 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 const NoPage = () => {
-  return <h1>This is page not found</h1>;
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/");
+  };
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto flex flex-col">
+        <span className="text-6xl text-red-600">Oops..</span>
+        <span className=" text-xl">Halaman yang anda cari tidak ditemukan</span>
+        <span
+          onClick={handleHome}
+          className="hover:underline text-blue-500 hover:cursor-pointer"
+        >
+          kembali ke halaman utama
+        </span>
+      </div>
+    </div>
+  );
 };
 export default NoPage;
